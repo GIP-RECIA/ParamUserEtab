aa484ceb1952a8ff282f3c849ffe0c1e3840d55b
Indah Juliani <indah.juliani@recia.fr>
2023-06-07 16:50:08 +0200

    update : methods saveStructure - backend

--- backend/src/main/java/com/example/dao/impl/LdapStructureDao.java
+++ backend/src/main/java/com/example/dao/impl/LdapStructureDao.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.naming.Name;
@@ -40,7 +39,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.HttpStatus;
-import org.springframework.lang.Nullable;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.filter.AndFilter;
 import org.springframework.ldap.filter.EqualsFilter;
@@ -53,7 +51,6 @@ import org.springframework.web.server.ResponseStatusException;
 import com.example.dao.IStructureDao;
 import com.example.dao.bean.IStructureFormatter;
 import com.example.model.Structure;
-import com.example.web.DTOStructure;
 
 /**
  * @author GIP RECIA 2013 - Maxime BOSSARD.
@@ -155,91 +152,44 @@ public class LdapStructureDao implements IStructureDao/*, InitializingBean*/{
 	}
 
 
-	// @Override
-	// public void saveStructure(DTOStructure struct) {
-
-	// 	// Validate input
-	// 	if (struct == null) {
-	// 		throw new IllegalArgumentException("Structure object cannot be null");
-	// 	}
-	// 	if (struct.getId() == null || struct.getId().isEmpty()) {
-	// 		throw new IllegalArgumentException("Structure ID cannot be null or empty");
-	// 	}
-	// 	if (struct.getStructCustomDisplayName() == null || struct.getStructCustomDisplayName().isEmpty()) {
-	// 		throw new IllegalArgumentException("Structure display name cannot be null or empty");
-	// 	}
-		
-	// 	// update displayName, logo, and siteWeb into Ldap 
-
-	// 	ModificationItem[] mods = new ModificationItem[3];
-	// 	Map<String, List<String>> otherAttrs = struct.getOtherAttributes();
-
-	// 	Attribute updateDNStruct = new BasicAttribute(this.structDisplayNameLdapAttr, struct.getStructCustomDisplayName());
-	// 	Attribute updateLogoStruct = null;
-	// 	Attribute updateSiteWebStruct = null; 
-	// 	for (Map.Entry<String, List<String>> entry : otherAttrs.entrySet()) {
-    //         String key = entry.getKey();
-    //         List<String> valueList = entry.getValue();
-
-    //         if (key.equals("ESCOStructureLogo") && valueList != null && !valueList.isEmpty() ) {
-	// 			updateLogoStruct = new BasicAttribute(valueList.get(0), struct.getStructLogo());
-    //         }
-    //         if (key.equals("ENTStructureSiteWeb") && valueList != null && !valueList.isEmpty() ) {
-	// 			updateSiteWebStruct = new BasicAttribute(valueList.get(0), struct.getStructSiteWeb());
-    //         }
-    //     }
-		
-	// 	final Name dn = buildDn(struct);
-	// 	mods[0] = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, updateDNStruct);
-	// 	mods[1] = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, updateLogoStruct);
-	// 	mods[2] = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, updateSiteWebStruct);
-
-	// 	this.ldapTemplate.modifyAttributes(dn, mods);
-	// 	log.info("Structure with ID {} updated in LDAP. Display name: {}. Logo: {}. Site web: {}.",
-	// 	struct.getId(), struct.getStructCustomDisplayName(), struct.getStructLogo(), struct.getStructSiteWeb());
-	// }
-
-
 	@Override
-	public void saveStructure(DTOStructure struct) {
+	public void saveStructure(String customName, String siteWeb, String logo, String id) {
+
 		// Validate input
-		if (struct == null) {
-			throw new IllegalArgumentException("Structure object cannot be null !");
-		}
-		if (struct.getId() == null || struct.getId().isEmpty()) {
-			throw new IllegalArgumentException("Structure ID cannot be null or empty !");
-		}
-			
-		// Update displayName, logo, and siteWeb into Ldap 
-		final Name dn = buildDn(struct);
-		List<ModificationItem> mods = new ArrayList<>();
-			
-		// this method is not ok !!! 
-		if (struct.getStructCustomDisplayName() != null || struct.getStructSiteWeb() != null) {
-			updateForm1(struct.getStructCustomDisplayName(), struct.getStructSiteWeb(), mods);
-		}
-		if (struct.getStructLogo() != null && !struct.getStructLogo().isEmpty()) {
-			log.info("struct logo is not null: logo par default");
-			//updateLogo(struct.getStructLogo(),  mods);
-		}
-			
-		this.ldapTemplate.modifyAttributes(dn, mods.toArray(new ModificationItem[mods.size()]));
-		log.info("Structure with ID {} updated in LDAP. Display name: {}. Logo: {}. Site web: {}.",
-			struct.getId(), struct.getStructCustomDisplayName(), struct.getStructLogo(), struct.getStructSiteWeb());
-	}
+	   if ( id == null || id.isEmpty()) {
+		   throw new IllegalArgumentException("Structure ID cannot be null or empty !");
+	   }
+		   
+	   // Update displayName, logo, and siteWeb into Ldap 
+	   final Name dn = buildDn(id);
+	   List<ModificationItem> mods = new ArrayList<>();
+   
+	   if (customName != null || siteWeb != null) {
+		   updateForm(customName, siteWeb, id, mods);
+	   }
+   
+	   if (logo != null) {
+		   //updateLogo(logo, id, mods);
+	   }
+   
+	   this.ldapTemplate.modifyAttributes(dn, mods.toArray(new ModificationItem[mods.size()]));
+	   log.info("Structure with ID {} updated in LDAP. Display name: {}. Logo: {}. Site web: {}.",
+		   id, customName, logo, siteWeb);
+   
+   }
 	
-	private Name buildDn(Structure struct) {
+	private Name buildDn(String idStruct) {
 		try {
-			if (struct.getId() != null) {
-				return LdapNameBuilder.newInstance(this.structDn.replace(this.structIdTemplate, struct.getId())).build();
+			if (idStruct != null) {
+				return LdapNameBuilder.newInstance(this.structDn.replace(this.structIdTemplate, idStruct)).build();
 			}
 		} catch (Exception e) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid structure ID: " + struct.getId(), e);
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid structure ID: " + idStruct, e);
 		}
 		return null;
 	}
 
-	//@SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 	private void updateForm1(String customName, String siteWeb, List<ModificationItem> mods) {
 
 		// update in database : sarapis 
@@ -261,91 +211,73 @@ public class LdapStructureDao implements IStructureDao/*, InitializingBean*/{
 	}
 
 	//@SuppressWarnings("unused")
-	private void updateLogo(String logo, List<ModificationItem> mods) {
-		// save in disk 
-		// update in database : sarapis 
+	private void updateLogo(String logo, String id, List<ModificationItem> mods) {
+		// save in database 
+		updateDataInDatabase(null, null, logo, id);
 
-		for (String attr : otherAttributes) {
+		// save in ldap
+		if ( logo != null ) {
+			for (String attr : otherAttributes) {
 
-			if (attr.equals("ESCOStructureLogo")) {
-				Attribute updLogo = new BasicAttribute(attr, logo);
-				mods.add(new ModificationItem(DirContext.REPLACE_ATTRIBUTE, updLogo));
+				if (attr.equals("ESCOStructureLogo")) {
+					Attribute updLogo = new BasicAttribute(attr, logo);
+					mods.add(new ModificationItem(DirContext.REPLACE_ATTRIBUTE, updLogo));
+				}
 			}
 		}
 	}
 
 
-	@SuppressWarnings("unused")
-	private void updateForm(DTOStructure dtoStruct) {
+	//@SuppressWarnings("unused")
+	private void updateForm(String customName, String siteWeb, String id, List<ModificationItem> mods) {
+		// save in database 
+		updateDataInDatabase(customName, siteWeb, null, id);
 
-		// update in database : sarapis 
-
-		// Validate input
-		if (dtoStruct == null) {
-			throw new IllegalArgumentException("Structure object cannot be null");
-		}
-		if (dtoStruct.getId() == null || dtoStruct.getId().isEmpty()) {
-			throw new IllegalArgumentException("Structure ID cannot be null or empty");
+		// save in ldap
+		if ( customName != null ) {
+			Attribute updCustomName = new BasicAttribute(this.structDisplayNameLdapAttr, customName);
+			mods.add(new ModificationItem(DirContext.REPLACE_ATTRIBUTE, updCustomName));
 		}
 
-		// update displayName and siteWeb into Ldap 
+		if ( siteWeb != null ) {
+			for (String attr : otherAttributes) {
 
-		ModificationItem[] mods = new ModificationItem[2];
-		Map<String, List<String>> otherAttrs = dtoStruct.getOtherAttributes();
-
-		Attribute updateDNStruct = new BasicAttribute(this.structDisplayNameLdapAttr, dtoStruct.getStructCustomDisplayName());
-		Attribute updateSiteWebStruct = null; 
-		for (Map.Entry<String, List<String>> entry : otherAttrs.entrySet()) {
-            String key = entry.getKey();
-            List<String> valueList = entry.getValue();
-
-            if (key.equals("ENTStructureSiteWeb") && valueList != null && !valueList.isEmpty() ) {
-				updateSiteWebStruct = new BasicAttribute(valueList.get(0), dtoStruct.getStructSiteWeb());
-            }
-        }
-		
-		final Name dn = buildDn(dtoStruct);
-		mods[0] = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, updateDNStruct);
-		mods[1] = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, updateSiteWebStruct);
-
-		this.ldapTemplate.modifyAttributes(dn, mods);
-		log.info("Structure with ID {} updated in LDAP. Display name: {}. Site web: {}.",
-		dtoStruct.getId(), dtoStruct.getStructCustomDisplayName(), dtoStruct.getStructSiteWeb());
+				if (attr.equals("ENTStructureSiteWeb")) {
+					Attribute updSiteWeb = new BasicAttribute(attr, siteWeb);
+					mods.add(new ModificationItem(DirContext.REPLACE_ATTRIBUTE, updSiteWeb));
+				}
+			}
+		}
 	}
 
-	@SuppressWarnings("unused")
-	private void updateImage(DTOStructure dtoStruct) {
-
-		// save in disk 
-		// update in database : sarapis  
-
-		// Validate input
-		if (dtoStruct == null) {
-			throw new IllegalArgumentException("Structure object cannot be null");
+	private void updateDataInDatabase(String customName, String siteWeb, String logo, String id) {
+		String updateQuery = "UPDATE astructure SET ";
+		List<Object> params = new ArrayList<>();
+	
+		if (customName != null) {
+			updateQuery += "astructure.nomCourt = ?, ";
+			params.add(customName);
 		}
-		if (dtoStruct.getId() == null || dtoStruct.getId().isEmpty()) {
-			throw new IllegalArgumentException("Structure ID cannot be null or empty");
+	
+		if (siteWeb != null) {
+			updateQuery += "astructure.siteWeb = ?, ";
+			params.add(siteWeb);
 		}
 
-		// update logo into Ldap 
-
-		ModificationItem[] mods = new ModificationItem[0];
-		Map<String, List<String>> otherAttrs = dtoStruct.getOtherAttributes();
-
-		Attribute updateLogoStruct = null;
-		for (Map.Entry<String, List<String>> entry : otherAttrs.entrySet()) {
-            String key = entry.getKey();
-            List<String> valueList = entry.getValue();
-
-            if (key.equals("ESCOStructureLogo") && valueList != null && !valueList.isEmpty() ) {
-				updateLogoStruct = new BasicAttribute(valueList.get(0), dtoStruct.getStructLogo());
-            }
-        }
+		if (logo != null) {
+			updateQuery += "astructure.logo = ?, ";
+			params.add(logo);
+		}
+	
+		// Remove the trailing comma and space from the updateQuery
+		updateQuery = updateQuery.substring(0, updateQuery.length() - 2);
 		
-		final Name dn = buildDn(dtoStruct);
-		mods[0] = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, updateLogoStruct);
-		this.ldapTemplate.modifyAttributes(dn, mods);
-		log.info("Structure with ID {} updated in LDAP. Logo : {}. Site web: {}.",
-		dtoStruct.getId(), dtoStruct.getStructLogo());
+		// Add the WHERE condition to the updateQuery
+		updateQuery += " WHERE id = ?";
+		params.add(id);
+
+		System.out.println("updateQuery : " + updateQuery);
+		System.out.println("params : " + params.toString());
+
 	}
 }
