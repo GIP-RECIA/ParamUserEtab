7fdb2bfd3e310d72a991089fa9bbbf733c808106
Indah Juliani <indah.juliani@recia.fr>
2023-08-18 12:04:49 +0200

    feat: unit test for API changeEtab and uploadLogo

--- backend/src/test/java/fr/recia/paramuseretab/web/rest/ParametabControllerTest.java
+++ backend/src/test/java/fr/recia/paramuseretab/web/rest/ParametabControllerTest.java
@@ -15,6 +15,7 @@
  */
 package fr.recia.paramuseretab.web.rest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -22,24 +23,41 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.mock.web.MockPart;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import fr.recia.paramuseretab.ParametabProjectApplication;
+import fr.recia.paramuseretab.model.Person;
 import fr.recia.paramuseretab.model.Structure;
+import fr.recia.paramuseretab.service.IImageUrlPath;
 import fr.recia.paramuseretab.service.IStructureService;
+import fr.recia.paramuseretab.service.IUserInfoService;
+import fr.recia.paramuseretab.service.IUserService;
 import fr.recia.paramuseretab.web.DTOStructure;
 import fr.recia.paramuseretab.web.Structure2DTOStructureImpl;
 
@@ -60,15 +78,34 @@ public class ParametabControllerTest {
     private IStructureService structureService;
 
     @Mock
+    private IUserService iUserService;
+
+    @Mock
+    private IUserInfoService iUserInfoService;
+
+    @Mock
     private Structure2DTOStructureImpl structure2dtoStructure;
 
     @InjectMocks
     private ParametabController parametabController;
 
+    @InjectMocks
+    private ChangeStructureRestController changeStructureRestController;
+
+    private Person person;
     private Structure struct;
     private Map<String, List<String>> otherAttrs = new HashMap<>();
+    private List<Map<String, String>> listMemberOf = new ArrayList<>();
+
+    private Map<String, String> groupe1 = new HashMap<>();
+    private Map<String, String> groupe2 = new HashMap<>();
+    private Map<String, String> groupe3 = new HashMap<>();
         
     private static ObjectMapper mapper = new ObjectMapper();
+
+    private MockMultipartFile file;
+    private String detailsJson;
+    private MockMultipartHttpServletRequestBuilder multipartReq; 
 
     @PostConstruct
     public void setup() {
@@ -78,11 +115,31 @@ public class ParametabControllerTest {
          * Initialize an etablissement ?? 
          * 
          */
-        
+        groupe1.put("idSiren", "410xx");
+        groupe1.put("etabName", "Etablissement 1");
+
+        groupe2.put("idSiren", "4250xx");
+        groupe2.put("etabName", "Etablissement 2");
+
+        groupe3.put("idSiren", "ABCD123");
+        groupe3.put("etabName", "Etablissement 3");
+
+        listMemberOf.add(groupe1);
+        listMemberOf.add(groupe2);
+        listMemberOf.add(groupe3);
+
         otherAttrs.put("ESCOStructureLogo", Arrays.asList("image0.png"));
         otherAttrs.put("ENTStructureSiteWeb", Arrays.asList("etab01aze.com"));
         struct = new Structure("ABCD123", "Etablissement A1", "Etab A1" , "Etab public", otherAttrs);
+        person = new Person("admin123", "4250xx", listMemberOf);
         mvc = MockMvcBuilders.standaloneSetup(parametabController).build();
+
+        file = new MockMultipartFile("file", "test.jpg", "image/jpeg", "test image".getBytes());
+
+        // Prepare the detailJson 
+        detailsJson = "{ \"id\":\"ABCD123\",\"name\":\"Etablissement A1\",\"displayName\":\"Etab A1\",\"description\":\"Etab public\",\"otherAttributes\":{\"ENTStructureSiteWeb\":[\"etab01aze.com\"],\"ESCOStructureLogo\":[\"image0.png\"]},\"structCustomDisplayName\":null,\"structLogo\":null,\"structSiteWeb\":null}";
+
+        multipartReq = MockMvcRequestBuilders.multipart("/parametab/fileUpload/"+ struct.getId());
     }
 
 
@@ -111,6 +168,10 @@ public class ParametabControllerTest {
   
         when(structureService.retrieveStructureById(struct.getId())).thenReturn(struct);
         when(structure2dtoStructure.toDTO(struct)).thenReturn(updateForm);
+        
+        doNothing().when(structureService).updateStructure(updateForm, updateForm.getStructCustomDisplayName(), updateForm.getStructSiteWeb(), null, updateForm.getId());
+
+        doNothing().when(structureService).invalidateStructureById(updateForm.getId());
 
         MockHttpServletRequestBuilder mockReq = MockMvcRequestBuilders.put("/parametab/updateV2/" + struct.getId())
             .contentType(MediaType.APPLICATION_JSON)
@@ -151,21 +212,74 @@ public class ParametabControllerTest {
     }
 
     @Test
-    public void testUpdateLogo() throws Exception {
+    public void testUploadFile_Success() throws Exception {
 
-        log.info("*** Test update logo ***");
+        ParametabController spyParam = spy(parametabController);
 
-        // Do like testUpdateForm : define DTOStructure, do the setter 
-        /**
-         * DTOStructure updateLogo = new DTOStructure();
-         *  updateLogo.setId(struct.getId());
-            updateLogo.setName(struct.getName());
-            updateLogo.setDisplayName(struct.getDisplayName());
-            updateLogo.setDescription(struct.getDescription());
-            updateLogo.setOtherAttributes(struct.getOtherAttributes());
-            updateLogo.setStructLogo("path/image");
-         */
+        IImageUrlPath newURL = mock(IImageUrlPath.class);
+        // doReturn(newURL).when(spyParam).calculNewImageUrlPath(any(DTOStructure.class), any(IImageUrlPath.class));
 
+        // Perform the file upload request 
+        multipartReq.part(new MockPart("details", detailsJson.getBytes()));
+
+        MvcResult res = mvc.perform(multipartReq.file(file)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        // Assert the response 
+        Assertions.assertEquals(HttpStatus.OK.value(), res.getResponse().getStatus());
+    }
+
+    @Test
+    public void testUploadFile_MissingDetailsJson() throws Exception {
+
+        // Mock the required dependencies and set up the test environnement 
+        MvcResult res = mvc.perform(multipartReq.file(file))
+                        .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                        .andReturn();
+
+        // Assert the response
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), res.getResponse().getStatus());
+
+    }
+
+    @Test
+    public void testUploadFile_ErrorSavingImage() throws Exception {
+
+        // Perform the file upload request
+        multipartReq.part(new MockPart("details", detailsJson.getBytes()));
+
+        // Mock the required dependencies and set up the test environnement 
+        MvcResult res = mvc.perform(multipartReq.file(file))
+                        .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                        .andReturn();
+        
+        // Assert the response 
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), res.getResponse().getStatus());
+
+    }
+
+    @Test
+    public void testChangeEtab() throws Exception {
+
+        log.info("*** Test changing the current etab ***");
+        String userID = null; 
+
+        when(iUserInfoService.getUserID()).thenReturn(userID);
+        when(structureService.retrieveStructureById(struct.getId())).thenReturn(struct);
+
+        doNothing().when(iUserService).changeCurrentStructure(userID, struct);
+        log.info("person : " + person.getUid());
+
+        MockHttpServletRequestBuilder mockReq = MockMvcRequestBuilders.put("/rest/change/" + struct.getId())
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .characterEncoding("UTF-8")
+        .content(mapper.writeValueAsString(struct));
+
+        mvc = MockMvcBuilders.standaloneSetup(changeStructureRestController).build();
+
+        mvc.perform(mockReq)
+            .andExpect(status().isOk())
+            .andDo(print());
 
     }
 
