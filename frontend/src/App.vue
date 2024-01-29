de2a359f04b4cea7376fab3e9f57a07f3c6e827f
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-01-29 14:53:28 +0100

    refactor: move front to another repository

--- frontend/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import './assets/detailList.css';
-import './assets/list.css';
-import 'cropperjs/dist/cropper.css';
-</script>
-
-<template>
-  <etab-ce></etab-ce>
-</template>
--- frontend/src/icons.ts
+++
@@ -1,4 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faChevronDown, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faMagnifyingGlass, faChevronDown);
